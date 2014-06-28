39cce3904aa74d24e9cdb3f0643123fbe3b0df21
Blake West <bwest87@gmail.com>
2014-06-27 18:59:58 -0700

    Got initial functionality working, and tests passing.
    - Adding attributes
    - Setting attributes through functions that get passed self, and index
    - Embedding other factories inside of any given factory
    - Throwing errors for a few invalid arg setups.

--- src/replicator.js
+++ src/replicator.js
@@ -1,11 +1,75 @@
 (function () {
+  var registry = {};
+  var traits = {};
+  var indicies = {};
+
+  var calculateProps = function (name, buildProps) {
+    var definedProps = registry[name];
+    // var definedTraits = traits[name]
+    var props = _.extend(definedProps, buildProps);
+
+    var funcProps = {};
+    var calculatedProps = {};
+    // put all non functions in the calculatedProps
+    _.each(props, function (val, key) {
+      if ( _.isFunction(val) ) {
+        funcProps[key] = val;
+      } else {
+        calculatedProps[key] = val;
+      }
+    });
+
+    // apply the traits
+
+    // now that all non funcs are copied, call the funcs
+    _.each(funcProps, function (val, key) {
+      calculatedProps[key] = val(calculatedProps, indicies[name]);
+    });
+
+    indicies[name]++;
+    return calculatedProps;
+  };
+
+  var define = function (name, props) {
+    // check args
+    if ( !_.isString(name) ) { throw new Error('A factory name is required.'); }
+    props = props || {};
+
+    // PUBLIC METHOD
+    var trait = function () {
+      return name;
+    };
+    // END PUBLIC METHOD
+
+    var factory = {
+      trait: trait,
+    };
+
+    // set to factory registry
+    registry[name] = props;
+    indicies[name] = 1;
+    // return self
+    return factory;
+  };
+
+
   var Replicator = {
+    define: define,
     build: function (name, props, copies) {
-      if (!name) { throw new Error('A factory name is required.'); }
-      console.log('build', arguments);
+      copies = copies || 1;
+      if ( !_.isString(name) ) { throw new Error('A factory name is required.'); }
+
+      var result = [];
+      _.each(_.range(copies), function(){
+        result.push(calculateProps(name, props));
+      });
+
+      if (copies === 1) { result = result[0]; }
+
+      return function() { return result; };
     },
-    define: function (name, props) {
-      console.log('define', arguments);
+    embed: function(name, props, copies) {
+      return this.build(name, props, copies)();
     },
     config:{}
   };
--- test/test.js
+++ test/test.js
@@ -1,31 +1,53 @@
 describe('Replicator', function() {
   describe('#define', function() {
+    describe("invalid arguments", function() {
+      it('should throw an error if no name is given', function() {
+        (function(){ Replicator.define(); }).should.throw(/name/);
+      });
+      it('should throw an error if name is not a string', function() {
+        (function(){ Replicator.define({}); }).should.throw(/name/);
+        (function(){ Replicator.define([]); }).should.throw(/name/);
+        (function(){ Replicator.define(4); }).should.throw(/name/);
+      });
+    });
     describe('when passed only a name', function() {
       beforeEach(function() {
         Replicator.define('user');
       });
-      it('should return an empty object', function() {
+      xit("should have #trait method", function() {
+      });
+      it('should set an empty object on the registry', function() {
         Replicator.build('user')().should.eql({});
       });
     });
-    describe('when not passed a name', function() {
-      it('should throw an error', function() {
-        (function(){ Replicator.build(); }).should.throw(/name/);
+    describe('when passed a hash', function() {
+      beforeEach(function() {
+        Replicator.define('user', {first: 'johnny', last: 'appleseed'});
       });
-    });
-    xdescribe('when passed a hash', function() {
       it('should set key value pairs', function() {
-
+        Replicator.build('user')().should.eql({first: 'johnny', last: 'appleseed'});
       });
       it('should embed other replicators', function() {
-
+        Replicator.define('bill', {amount: 50});
+        Replicator.define('user', {first: 'johnny', last: 'appleseed', bill: Replicator.embed('bill')});
+        Replicator.build('user')().should.eql({first: 'johnny', last: 'appleseed', bill: {amount: 50}});
       });
-      describe('should accept functions as keys', function() {
-        it('should pass the obj and an index as arguments', function() {
-
+      describe('should accept functions as keys', function(done) {
+        beforeEach(function(){
+          Replicator.define('user', {
+            name: "joe",
+            email: function(props, index) {
+              return props.name + index + "@gmail.com";
+            }
+          });
         });
-        it('should set the returned value as a property', function() {
-
+        it('should pass the props and an index', function() {
+          Replicator.build('user')().email.should.eql("joe1@gmail.com");
+        });
+        it('should increment the index', function() {
+          Replicator.build('user')().email.should.eql("joe1@gmail.com");
+          Replicator.build('user')().email.should.eql("joe2@gmail.com");
+          Replicator.build('user')().email.should.eql("joe3@gmail.com");
         });
       });
     });
